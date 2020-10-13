28d35408e8d7a9e58430ab62b3e36287b3c147d8
leebin <libinjava@163.com>
2020-10-13 16:35:04 +0800

    修改

--- src/test/java/com/hujia/ebfa/EbfaApplicationTests.java
+++ src/test/java/com/hujia/ebfa/EbfaApplicationTests.java
@@ -1,13 +1,33 @@
 package com.hujia.ebfa;
 
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.util.FileUtils;
+import com.hujia.ebfa.domain.Assets;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class EbfaApplicationTests {
 
+    private List<Assets> data() {
+        List<Assets> list = new ArrayList<Assets>();
+        for (int i = 0; i < 10; i++) {
+            Assets data = new Assets();
+            data.setName("字符串" + i);
+            list.add(data);
+        }
+        return list;
+    }
+
     @Test
     void contextLoads() {
+
+        String fileName =  "C:\\Users\\Administrator\\Desktop\\simpleWrite.xlsx";
+        EasyExcel.write(fileName, Assets.class).sheet("模板").doWrite(data());
     }
 
 }
