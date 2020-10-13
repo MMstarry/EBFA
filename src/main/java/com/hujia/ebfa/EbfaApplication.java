faba1b13d18995522c251e1a12c7032d9dbe57ac
1668846604@qq.com <1668846604@qq.com>
2020-10-13 14:40:27 +0800

    first

---
+++ src/main/java/com/hujia/ebfa/EbfaApplication.java
@@ -0,0 +1,13 @@
+package com.hujia.ebfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbfaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbfaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hujia/ebfa/EbfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hujia.ebfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbfaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
